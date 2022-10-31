fab630600091a05ae196a5851a13f736c9bd716f
Patrick Dobry <patrickdobry@gmail.com>
2022-10-30 22:53:40 -0500

    Stil buggy

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,6 +26,8 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             # Watch for keyboard and mouse events.
+            self._check_events()
+    def _check_events(self):        
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
